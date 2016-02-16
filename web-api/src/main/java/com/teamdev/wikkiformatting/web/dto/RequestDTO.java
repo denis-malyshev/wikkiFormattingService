c1ec65917e8e64878a0dba7f2924f2e84848cd4f
Denis Malyshev <denis.malyshev@teamdev.com>
2016-02-17 00:27:49 +0200

    Moved RequestDTO to separated module

---
+++ web-api/src/main/java/com/teamdev/wikkiformatting/web/dto/RequestDTO.java
@@ -0,0 +1,10 @@
+package com.teamdev.wikkiformatting.web.dto;
+
+public class RequestDTO {
+
+    public final String text;
+
+    public RequestDTO(String text) {
+        this.text = text;
+    }
+}
