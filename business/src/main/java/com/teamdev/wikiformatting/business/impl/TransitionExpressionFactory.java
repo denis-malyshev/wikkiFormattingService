84c5cfcb92ec72c5368343d8bffb2de9d9fe458c
Denis Malyshev <denis.malyshev@teamdev.com>
2016-02-17 15:01:16 +0200

    Fixed heading formatting

--- business/src/main/java/com/teamdev/wikiformatting/business/impl/TransitionExpressionFactory.java
+++ business/src/main/java/com/teamdev/wikiformatting/business/impl/TransitionExpressionFactory.java
@@ -5,6 +5,7 @@ import java.util.*;
 public abstract class TransitionExpressionFactory {
 
     private static final Set<TransitionExpression> WIKI_TO_HTML = new LinkedHashSet<TransitionExpression>() {{
+        add(new TransitionExpression("\\[\\[(.+)\\]\\[(.+)\\]\\]", "<a href=$2>$1</a>"));
         add(new TransitionExpression("={4}(.*)={4}", "<h4>$1</h4>"));
         add(new TransitionExpression("={3}(.*)={3}", "<h3>$1</h3>"));
         add(new TransitionExpression("={2}(.*)={2}", "<h2>$1</h2>"));
@@ -13,7 +14,6 @@ public abstract class TransitionExpressionFactory {
         add(new TransitionExpression("__(.*)__", "<b><i>$1</i></b>"));
         add(new TransitionExpression("_(.*)_", "<i>$1</i>"));
         add(new TransitionExpression("\\*(.*)\\*", "<b>$1</b>"));
-        add(new TransitionExpression("\\[\\[(.+)\\]\\[(.+)\\]\\]", "<a href=$2>$1</a>"));
     }};
 
     public static Set<TransitionExpression> getWikiToHtml() {
